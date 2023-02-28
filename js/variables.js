d9d225167b35ac2ca4c4c804e419b1b72aabb966
facundo cajal <facundocajal199@gmail.com>
2023-02-28 20:43:40 -0300

    primer push

---
+++ js/variables.js
@@ -0,0 +1,35 @@
+// comentario unna sola linea 
+
+/*comentario de 
+varias lineas*/
+
+//como declarar una variable (let y const)
+
+let URLCampus = "https://universe.rollingcodeschool.com/live/videos/62d9a9d43f5cdf742e84e67f"
+
+let pelicula = "Titanic";
+
+//mostrar una variable por consola
+
+console.log("Hola mundo")
+console.log(pelicula);
+
+
+//mostrar una variable por consola
+
+console.log("hola mundo, voy a ver " + pelicula +"3D");
+console.log(pelicula);
+//mostrar una ventana en el html
+document.write("<h1>estrenos</h1>");
+document.write(`<p>voy a ver ${pelicula} 3D<p/>`)
+
+
+//mensaje de alerta 
+alert("This is an alert message")
+document.write(URLCampus)
+
+//modificar valor de una variable
+
+pelicula = "Venom";
+document.write(`<p>voy a ver ${pelicula
+} 3D<p/>`)
